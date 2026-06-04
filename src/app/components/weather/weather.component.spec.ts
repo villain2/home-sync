e55dd9d489e167392ce4d85d76e3d6241f9e8181
villain2 <villain2@users.noreply.github.com>
2026-06-04 17:39:59 -0400

    Updating tests.

--- src/app/components/weather/weather.component.spec.ts
+++ src/app/components/weather/weather.component.spec.ts
@@ -2,21 +2,41 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CommonModule } from '@angular/common';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-
+import { of } from 'rxjs';
 import { WeatherComponent } from './weather.component';
+import { WeatherService } from 'src/app/services/weather.service';
+import { ThermostatService } from 'src/app/services/thermostat.service';
+
+const mockWeather = {
+  location: 'Test',
+  currentWeather: 'Sunny',
+  currentIcon: '',
+  currentTemp: '72',
+  forecast: []
+};
 
 describe('WeatherComponent', () => {
   let component: WeatherComponent;
   let fixture: ComponentFixture<WeatherComponent>;
+  let weatherSpy: jasmine.SpyObj<WeatherService>;
+  let thermoSpy: jasmine.SpyObj<ThermostatService>; 
 
   beforeEach(async(() => {
+    weatherSpy = jasmine.createSpyObj('WeatherService', ['getWeatherData']);
+    thermoSpy = jasmine.createSpyObj('ThermostatService', ['sendRequest']);
+    weatherSpy.getWeatherData.and.returnValue(of(mockWeather));
+    thermoSpy.sendRequest.and.returnValue(of({}));
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, CommonModule],
-      declarations: [ WeatherComponent ],
+      declarations: [WeatherComponent],
+      providers: [
+        { provide: WeatherService, useValue: weatherSpy },
+        { provide: ThermostatService, useValue: thermoSpy }
+      ],
       schemas: [NO_ERRORS_SCHEMA]
-    })
-    .compileComponents();
-  }));
+  }).compileComponents();
+
+}));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(WeatherComponent);
@@ -24,7 +44,16 @@ describe('WeatherComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create and load weather', () => {
     expect(component).toBeTruthy();
+    expect(component.currentTemp).toBe('72');
+    expect(component.currentConditions).toBe('Sunny');
   });
+
+  it('setActive toggles and calls thermostat', () => {
+    component.setActive('heat');
+    expect(component.activeButton).toBe('heat');
+    expect(component.fanActive).toBeTrue();
+  });
+
 });
--- src/app/services/thermostat.service.spec.ts
+++ src/app/services/thermostat.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ThermostatService } from './thermostat.service';
 
 describe('ThermostatService', () => {
-  let service: ThermostatService;
+let service: ThermostatService;
+let httpMock: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ThermostatService);
-  });
+beforeEach(() => {
+TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
+service = TestBed.inject(ThermostatService);
+httpMock = TestBed.inject(HttpTestingController);
+});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+afterEach(() => httpMock.verify());
+
+it('should be created', () => expect(service).toBeTruthy());
+
+it('should error when endpoint is empty', (done) => {
+service.sendRequest('').subscribe({
+next: () => fail('expected error'),
+error: (err: Error) => { expect(err.message).toContain('Thermostat endpoint is required'); done(); }
+});
+});
+
+it('should GET the thermostat endpoint', () => {
+service.sendRequest('/thermostat_control.php?action=fan_on').subscribe();
+const req = httpMock.expectOne(r => r.url.includes('thermostat_control.php'));
+expect(req.request.method).toBe('GET');
+req.flush({});
+});
+
 });
